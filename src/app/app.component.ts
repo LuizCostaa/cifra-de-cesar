63b7f6e3355e46865e01b042a16bde9318edbca8
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-06-27 03:04:28 -0300

    Criação do algoritmo da cifra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  obj: any = {
-    texto: '',
-    chave: ''
-  };
-
-  constructor() { }
-
   onSubmit(form): void {
-    console.log(form);
+    const texto: string = form.value.texto.toUpperCase();
+    const chave: string = form.value.chave.toUpperCase();
+    const resultOutput = [];
+
+    [...texto].forEach((letter: string) => {
+      resultOutput.push(this.calculateIndexOfAscii(letter.charCodeAt(0), chave.charCodeAt(0)));
+    });
+
+    // console.log(resultOutput.join('').replace(';', ' '));
+  }
+
+  calculateIndexOfAscii(letter: number, asciiKeyIndex: number): string {
+    asciiKeyIndex = asciiKeyIndex - 65;
+
+    console.log((letter - 65 + asciiKeyIndex) % 26);
+    return String.fromCharCode((letter - 65 + asciiKeyIndex) % 26 + 65);
   }
 }
